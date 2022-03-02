f7072b12bb2087fdab6a819ee46d54e60658606e
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2022-03-02 14:53:16 -0700

    Update insultgen.py

--- insultgen/insultgen.py
+++ insultgen/insultgen.py
@@ -1,11 +1,18 @@
 import random
+kg = True 
 person = ["Dad","mom","sister","brother","aunt","uncle","grandma","grandpa"]
 insultw1 = ["smelly ","stinky ","sucky ","stupid ","ugly ","dumb ","arrogant ","bossy ","boring ","fussy ","grumpy ","moody ","toasty "]
-insultst = ["You are a ","ur a ","you suck ","I hate you becuase ur a "]
+insultst = ["You are a ","ur a ","you suck ","I hate you becuase ur a ","Ew, a ","well well well if it isnt the ","woah, look at the ","AAAA ITS A "]
 insultw2 = ["peasant","idiot","coward","baby sniffer","scallywag","leg licker ","milk before cereal eater ","uncooked spagetti eater","frozen pasta gulper","knee taster","fool","dummy","always cold idiot","grandpa stealer"]
-awnser = input("Are you ready? (y/n)")
-if awnser == "n":
+while kg == True:
+  awnser = input("Are you ready? (y/n)")
+  if awnser == "n":
     print("ok bye")
-else:
+  else:
     print(random.choice(insultst) + random.choice(insultw1) + random.choice(insultw2)) 
     print("There ya go!")
+    kga = input("more? (y/n)")
+    if kga == "n":
+        kg = False
+    
+    
