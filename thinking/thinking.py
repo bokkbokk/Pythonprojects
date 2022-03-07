7d0198caa922ecb93249ce1c14e8f40a2a8279ab
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2022-03-07 12:19:13 -0700

    Create thinking.py

---
+++ thinking/thinking.py
@@ -0,0 +1,11 @@
+import random
+import time
+import turtle
+print("Im thinking about... ")
+words1 = ["eating","drinking","playing with","using","comsuming","sitting on","sleeping on","cleaning","shooting","pleading with","pleasing","smelling","teaching","spitting on","killing","smiling at","yelling at","attacking","calming","singing to","reading to","sniffing","touching","stabbing","setting fire to","dumping water on"]
+words2 = [" a silly "," a gross "," a nasty "," a clean "," a smart "," a dumb "," a slippery "," a blood stained "," a hairy "," a moody "," a pink "," a pleasant "," a peeled "," a scary "," a spooky "," a killer "," a greasy "," a smooth "," a funny "," a unfunny "," a fresh "," a sad "," a happy "," a angry "]
+words3 = ["dog","librarian","book","toilet","sink","shoe","air purifier","pants","paper","spatula","microwave","glasses","socks","hair","eyeball","tree","backpack","clock","building","person","dad","cat","mom","brother","sister","baby","desk","beanie","hat","computer"]
+print(random.choice(words1) + random.choice(words2) + random.choice(words3))
+ 
+
+       
